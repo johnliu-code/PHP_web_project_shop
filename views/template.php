9467d3d52628a6a4f5863d371a5024c8af0650be
John Liu <aimingfuture@gmail.com>
2022-11-17 18:21:49 -0500

    1. Fix the url useing <base> cause problem could not found the page in    copied server 2. The way thinking was missing considering commom case for rebuild web    site and only test on local works. Avoid this in the future...

--- views/template.php
+++ views/template.php
@@ -3,7 +3,6 @@
     session_start();   
 ?>
 
-<base href="http://phptest/TP3_YL_LYJ/" />
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="./public/style.css">
     <title><?= $title ?></title>
 </head>
 <body>
